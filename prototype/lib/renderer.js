32ef1274d8c317f7de15ae1793f7452a006c41da
artygus <hero@engineeriam.net>
2015-05-24 00:23:13 +0300

    wall renderer

---
+++ prototype/lib/renderer.js
@@ -0,0 +1,55 @@
+function Renderer(el) {
+  this.node = el;
+  this.unit = 100; // unit vector in px
+}
+
+Renderer.prototype = {
+  // @param {Array} coord - [x,y]
+  // @param {String} dir - direction: n,e,s,w
+  // @param {String} cls - CSS class
+  wall: function(coord, dir, cls) {
+    var wallEl = document.createElement('div'),
+        x = coord[0] * this.unit,
+        y = coord[1] * this.unit,
+        origin = [0, 0],
+        rotate3 = [0, 0, 0],
+        translate3 = [0, 0, 0];
+
+    switch(dir) {
+      case 'n':
+        origin = [50, 0];
+        rotate3 = [1, 0, 0];
+        break;
+      case 'e':
+        x += this.unit;
+        origin = [0, 50];
+        rotate3 = [0, 1, 0];
+        translate3 = [-this.unit, 0, 0];
+        break;
+      case 's':
+        y += this.unit;
+        origin = [50, 0];
+        rotate3 = [1, 0, 0];
+        rotate3 = [1, 0, 0];
+        break;
+      case 'w':
+        origin = [0, 50];
+        rotate3 = [0, 1, 0];
+        translate3 = [-this.unit, 0, 0];
+        break;
+    }
+
+    wallEl.style.position = 'absolute';
+    wallEl.style.left = x + 'px';
+    wallEl.style.top = y + 'px';
+
+    wallEl.style.width = this.unit + 'px';
+    wallEl.style.height = this.unit + 'px';
+
+    wallEl.style.transform = 'rotate3d(' + rotate3 + ',90deg) translate3d(' + translate3.map(function(p) { return p + 'px' }) + ')';
+    wallEl.style.transformOrigin = origin[0] + '% ' + origin[1] + '%';
+
+    wallEl.className = cls;
+    this.node.appendChild(wallEl);
+  }
+}
