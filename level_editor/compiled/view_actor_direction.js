71670780a5c7de644608e2d11a65a657a2ab397b
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2016-10-03 11:06:55 +0300

    Actor direction form state

--- level_editor/compiled/view_actor_direction.js
+++ level_editor/compiled/view_actor_direction.js
@@ -5,7 +5,8 @@
  */
 
 (function() {
-  var __hasProp = {}.hasOwnProperty,
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   levelEditor.view.ActorDirection = (function(_super) {
@@ -15,12 +16,105 @@
 
     ActorDirection.VID = "view-leditor-actor-direction";
 
-    function ActorDirection(view, app) {
-      this.view = view;
+    function ActorDirection(el, app) {
+      this.el = el;
       this.app = app;
+      this.checkRadioByDirection = __bind(this.checkRadioByDirection, this);
+      this.uncheckAllRadios = __bind(this.uncheckAllRadios, this);
+      this.getRadioByDirection = __bind(this.getRadioByDirection, this);
+      this.setActorIdText = __bind(this.setActorIdText, this);
+      this.stateNoCellSelected = __bind(this.stateNoCellSelected, this);
+      this.stateActor = __bind(this.stateActor, this);
+      this.stateNoActor = __bind(this.stateNoActor, this);
+      this.enableWorldDirectionRadios = __bind(this.enableWorldDirectionRadios, this);
+      this.disableWorldDirectionRadios = __bind(this.disableWorldDirectionRadios, this);
+      this.getRadios = __bind(this.getRadios, this);
+      this.logicUpdateDisableStateOnCellSelected = __bind(this.logicUpdateDisableStateOnCellSelected, this);
       ActorDirection.__super__.constructor.apply(this, arguments);
       console.log(this.DT, "Init.");
+      this.logicUpdateDisableStateOnCellSelected();
     }
+
+    ActorDirection.prototype.logicUpdateDisableStateOnCellSelected = function() {
+      var updateState;
+      updateState = (function(_this) {
+        return function(s) {
+          var apos;
+          if (s === null) {
+            return _this.stateNoCellSelected();
+          } else if ((apos = _this.app.data.get("level-actors").getActorOnCell(s)) != null) {
+            return _this.stateActor(apos);
+          } else {
+            return _this.stateNoActor();
+          }
+        };
+      })(this);
+      $(this.app.data).asEventStream(this.app.data.s.I_DATA_CHANGED).filter(function(v) {
+        return v.key === "selected-cell";
+      }).onValue((function(_this) {
+        return function(v) {
+          return updateState(v.value);
+        };
+      })(this));
+      return updateState(this.app.data.get("selected-cell"));
+    };
+
+    ActorDirection.prototype.getRadios = function() {
+      return this.el.find("[type=radio]");
+    };
+
+    ActorDirection.prototype.disableWorldDirectionRadios = function() {
+      this.el.addClass("-disabled");
+      return this.getRadios().attr({
+        disabled: true
+      });
+    };
+
+    ActorDirection.prototype.enableWorldDirectionRadios = function() {
+      this.el.removeClass("-disabled");
+      return this.getRadios().attr({
+        disabled: null
+      });
+    };
+
+    ActorDirection.prototype.stateNoActor = function() {
+      this.disableWorldDirectionRadios();
+      this.uncheckAllRadios();
+      return this.setActorIdText("No actor");
+    };
+
+    ActorDirection.prototype.stateActor = function(apos) {
+      this.enableWorldDirectionRadios();
+      this.setActorIdText("Actor#id");
+      return this.checkRadioByDirection(apos.dir);
+    };
+
+    ActorDirection.prototype.stateNoCellSelected = function() {
+      this.disableWorldDirectionRadios();
+      this.uncheckAllRadios();
+      return this.setActorIdText("No cell");
+    };
+
+    ActorDirection.prototype.setActorIdText = function(text) {
+      return this.el.find("[actor-id]").text(text);
+    };
+
+    ActorDirection.prototype.getRadioByDirection = function(dir) {
+      return this.el.find("#actor-direction-" + (dir.toLowerCase()));
+    };
+
+    ActorDirection.prototype.uncheckAllRadios = function() {
+      return this.getRadios().attr({
+        checked: null
+      });
+    };
+
+    ActorDirection.prototype.checkRadioByDirection = function(dir) {
+      this.uncheckAllRadios();
+      return this.getRadioByDirection(dir).prop({
+        checked: true
+      });
+    };
 
     return ActorDirection;
 
--- level_editor/view_actor_direction.coffee
+++ level_editor/view_actor_direction.coffee
@@ -8,6 +8,73 @@ class levelEditor.view.ActorDirection extends levelEditor.Object
 
   @VID: "view-leditor-actor-direction"
 
-  constructor: (@view, @app)->
+  constructor: (@el, @app)->
     super
     console.log @DT, "Init."
+
+    @logicUpdateDisableStateOnCellSelected()
+
+
+  # section: Logic
+
+  logicUpdateDisableStateOnCellSelected: =>
+    updateState = (s)=>
+      if s == null
+        @stateNoCellSelected()
+      else if (apos = @app.data.get("level-actors").getActorOnCell(s))?
+        @stateActor(apos)
+      else
+        @stateNoActor()
+
+    $(@app.data)
+      .asEventStream(@app.data.s.I_DATA_CHANGED)
+      .filter((v)-> v.key == "selected-cell")
+      .onValue (v)=> updateState(v.value)
+
+    updateState @app.data.get("selected-cell")
+
+
+  # section: View
+
+  getRadios: => @el.find("[type=radio]")
+
+  disableWorldDirectionRadios: =>
+    @el.addClass "-disabled"
+    @getRadios().attr(disabled: true)
+
+  enableWorldDirectionRadios: =>
+    @el.removeClass "-disabled"
+    @getRadios().attr(disabled: null)
+
+  stateNoActor: =>
+    @disableWorldDirectionRadios()
+    @uncheckAllRadios()
+    @setActorIdText("No actor")
+
+  # @param {dataTypes.ActorPosition} apos
+  stateActor: (apos)=>
+    @enableWorldDirectionRadios()
+    @setActorIdText("Actor#id")
+    @checkRadioByDirection(apos.dir)
+
+  stateNoCellSelected: =>
+    @disableWorldDirectionRadios()
+    @uncheckAllRadios()
+    @setActorIdText("No cell")
+
+  # @param {String}
+  setActorIdText: (text)=>
+    @el.find("[actor-id]").text(text)
+
+  # @param {dataTypes.WorldDirection}
+  getRadioByDirection: (dir)=>
+    @el.find("#actor-direction-#{dir.toLowerCase()}")
+
+  uncheckAllRadios: =>
+    @getRadios().attr(checked: null)
+
+  checkRadioByDirection: (dir)=>
+    @uncheckAllRadios()
+    @getRadioByDirection(dir).prop(checked: true)
+
+
